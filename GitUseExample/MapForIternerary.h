b3e14739a4310ac9634ec2aa2dfb94821f2ec67c
wuxiaowei <wuxiaowei@outlook.com>
2013-05-18 16:50:12 +0800

    for map

---
+++ GitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by xiaowei wu on 13-5-18.
+//  Copyright (c) 2013年 xiaowei wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ GitUseExample/MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  GitUseExample
+//
+//  Created by xiaowei wu on 13-5-18.
+//  Copyright (c) 2013年 xiaowei wu. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
